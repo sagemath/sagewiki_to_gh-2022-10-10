80dcfe11c6332a3769151f51975e75dc704451e0
slabbe <slabqc@gmail.com>
2009-07-16 13:23:09 +0000

    Change to file SébastienLabbé/JoursSageUQAM/sequence.py

---
+++ SébastienLabbé/JoursSageUQAM/sequence.py
@@ -0,0 +1,19 @@
+def fibo(n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+
+def yo():
+    print 'Je suis la!'
+
+def yo2():
+    print 'Je suis la2!'
+
+def yo3():
+    print 'Je suis la3!'
+
+def yo4(): print 'etest'
